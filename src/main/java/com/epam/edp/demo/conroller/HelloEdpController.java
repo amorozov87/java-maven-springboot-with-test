0ef37859660a2eec5954249549c6c720f546f646
Roman Martseniuk <roman_martseniuk@epam.com>
2018-11-20 15:37:05 +0200

    Add incorrect status for demo

--- src/main/java/com/epam/edp/demo/conroller/HelloEdpController.java
+++ src/main/java/com/epam/edp/demo/conroller/HelloEdpController.java
@@ -2,7 +2,9 @@ package com.epam.edp.demo.conroller;
 
 import com.epam.edp.demo.service.HelloEdpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,6 +21,7 @@ public class HelloEdpController {
     }
 
     @GetMapping(value = "/api/hello")
+    @ResponseStatus(HttpStatus.CREATED)
     public String hello() {
         return edpService.getHelloWorld();
     }
